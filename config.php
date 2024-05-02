537bc97628834531caac2b68cbb6bf4b93e72838
Swapneel Raosaheb Khandagale AMIE <noblesse@noblesseinfosec.com>
2024-05-02 05:56:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'augustoandro');
-define('DB_PASSWORD', 'manjaro');
-define('DB_DATABASE', 'groww_login');
+define('DB_USERNAME', 'USERNAME');
+define('DB_PASSWORD', 'PASSWORD');
+define('DB_DATABASE', 'DB_NAME');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
